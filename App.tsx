12620aabf33db5c2afc5d384e6b61c084b588343
Simon <phillipe.simon@gmail.com>
2024-02-03 00:25:06 -0300

    Using custom fonts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
 import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import theme from './src/theme';
 
 import { Groups } from '@screens/Groups';
+import { StatusBar } from 'expo-status-bar';
+import { ActivityIndicator } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      <StatusBar style='light' />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
