394371c8210b9f39fdc621d3ef912167bce07cde
Simon <phillipe.simon@gmail.com>
2024-02-04 08:08:59 -0300

    Listing the groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,8 +2,13 @@ import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 export function Groups() {
+    const [groups, setGroups] = useState(['Dark', 'Simon', 'Phillipe'])
+
+
     return (
         <Container>
             <Header />
@@ -12,9 +17,15 @@ export function Groups() {
                 subtitle="Play with your team"
             />
 
-            <GroupCard
-                title="Team of the beast"
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <GroupCard title={item} />
+                )}
+
             />
+
         </Container>
     )
 }
