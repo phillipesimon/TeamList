9f7e43903ee95e1fa992c7b8e0b4e3c37d3257c1
Simon <phillipe.simon@gmail.com>
2024-02-04 05:24:05 -0300

    Creating the highlight component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,15 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
+import { Highlight } from "@components/Highlight";
 
 export function Groups() {
     return (
         <Container>
             <Header />
+            <Highlight
+                title="Team"
+                subtitle="Play with your team"
+            />
         </Container>
     )
 }
