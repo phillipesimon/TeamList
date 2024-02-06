b01a04387306c9caf67ba5b31f28ca11ceb0c1ea
Simon <phillipe.simon@gmail.com>
2024-02-06 06:47:24 -0300

    Listing the filters

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,12 +1,19 @@
-import { Header } from "@components/Header";
-import { Container, Form } from "./styles";
-import { Highlight } from "@components/Highlight";
-import { ButtonIcon } from "@components/ButoonIcon";
+import { FlatList } from "react-native";
+import { useState } from "react";
+
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { ButtonIcon } from "@components/ButoonIcon";
 
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
+
+    const [team, setTeam] = useState('TEAM A')
+    const [players, setPlayers] = useState(['Simon', "Phillipe", "Martins", "Costa"])
+
     return (
         <Container>
             <Header showBackButton />
@@ -18,9 +25,28 @@ export function Players() {
                 <Input placeholder="Username" autoCorrect={false} />
                 <ButtonIcon icon="add" />
             </Form>
-            <Filter
-                title="A Team"
-            />
+
+
+            <HeaderList>
+
+                <FlatList
+                    data={['TEAM A', 'TEAM B']}
+                    keyExtractor={item => item}
+                    renderItem={({ item }) => (
+                        <Filter
+                            title={item}
+                            isActive={item === team}
+                            onPress={() => setTeam(item)}
+                        />
+                    )}
+                    horizontal
+                />
+
+                <NumberOfPlayers>
+                    {players.length}
+                </NumberOfPlayers>
+            </HeaderList>
+
 
         </Container>
     )
