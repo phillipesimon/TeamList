5576ce5bd38e248d9d201616a26fbb9f55ab93d2
Simon <phillipe.simon@gmail.com>
2024-02-07 11:07:20 -0300

    Implementing the return

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,8 +3,15 @@ import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
 import { Container, Content, Icon } from './styles'
+import { useNavigation } from '@react-navigation/native'
 
 export function NewGroup() {
+    const navigation = useNavigation()
+
+    function handleNew() {
+        navigation.navigate('players', { group: 'Rocket' })
+    }
+
     return (
         <Container>
             <Header showBackButton />
@@ -16,7 +23,7 @@ export function NewGroup() {
                     subtitle="Create a new group to add people"
                 />
                 <Input placeholder="Team name" />
-                <Button title="Create" />
+                <Button title="Create" onPress={handleNew} />
             </Content>
         </Container>
     )
