f992e83c79badfece2c7c41a6825357100152041
Simon <phillipe.simon@gmail.com>
2024-02-08 10:06:57 -0300

    Identifying the focus on the screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,19 +3,35 @@ import { GroupCard } from '@components/GroupCard'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { ListEmpty } from '@components/ListEmpty/inedx'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import { FlatList } from 'react-native'
 import { Container } from './styles'
-import { useNavigation } from '@react-navigation/native'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
+import { groupGetAll } from '@storage/group/groupGetAll'
 
 export function Groups() {
-    const [groups, setGroups] = useState([])
+    const [groups, setGroups] = useState<string[]>([])
 
     const navigation = useNavigation()
 
     function handleNavigation() {
         navigation.navigate('new')
     }
+
+    async function fetchGroups() {
+        try {
+            const data = await groupGetAll()
+            setGroups(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useFocusEffect(
+        useCallback(() => {
+            fetchGroups()
+        }, [])
+    )
 
     return (
         <Container>
